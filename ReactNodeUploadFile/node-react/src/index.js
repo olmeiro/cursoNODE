7397dc1a7d0b7606d68b96dbcaa739c717e17e5f
Olmeiro Orozco <olme.orozco@gmail.com>
2021-09-22 14:26:36 -0500

    first commit

---
+++ ReactNodeUploadFile/node-react/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import FileUpload from "./FileUpload";
+
+function App() {
+  return (
+    <div>
+      <h2>react node js file upload example</h2>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
